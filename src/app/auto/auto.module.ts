6fd9d6dd427e9438b9bdaf3005b3052e5a9928a3
vadim.loginov <v_loginov@gcore.lu>
2020-10-16 15:46:58 +0300

    current

---
+++ src/app/auto/auto.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AutoRoutingModule } from './auto-routing.module';
+import { AutoComponent } from './auto.component';
+
+
+@NgModule({
+  declarations: [AutoComponent],
+  imports: [
+    CommonModule,
+    AutoRoutingModule
+  ]
+})
+export class AutoModule { }
---
+++ src/app/messages/sent-messages/sent-messages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-send-messages',
+  templateUrl: './sent-messages.component.html',
+  styleUrls: ['./sent-messages.component.scss']
+})
+export class SentMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
