2ebb460c885ed8794ccb781026264cbe72d31dbb
vadim.loginov <v_loginov@gcore.lu>
2020-10-16 15:46:40 +0300

    current

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { SentMessagesComponent } from './sent-messages/sent-messages.component';
 import {MessagesRoutingModule} from './messages-routing.module';
 import { DraftMessagesComponent } from './draft-messages/draft-messages.component';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
 @NgModule({
   declarations: [SentMessagesComponent, DraftMessagesComponent],
-  imports: [
-    CommonModule,
-    MessagesRoutingModule,
-  ]
+    imports: [
+        CommonModule,
+        MessagesRoutingModule,
+        MatIconModule,
+    ]
 })
 export class MessagesModule { }
